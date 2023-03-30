9b1292ffe0c7adb0222157633f643fc7ce07ab87
dipak <sarkar3115@gmail.com>
2023-03-30 14:39:24 +0530

    d

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './images/logo.svg';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <article className='coming-soon'>
+      <section className='coming-soon-logo'>
+        <img src={logo} alt=''/>
+      </section>
+      <section className='coming-soon-hero'>
+      </section>
+      <section className='coming-soon-msg'>
+        <h1><span>We're</span> coming soon</h1>
+        <p>Hello fellow shoppers! We're currently building our new fashion store. Add your email below to stay up-to-date with announcements and our launch deals.</p>
+        <section className='cs-email-input'>
+          <input type="email" id="email" name="email" placeholder=' Email Address'></input>
+          <button> &#62;</button>
+        </section>
+      </section>
+    </article>
+     
   );
 }
 
